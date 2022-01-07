8e26a56db287c03fc392603f023e40c7ed372616
mwendaB <brian.mwenda@student.moringaschool.com>
2022-01-07 13:22:08 +0300

    Updates

--- src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Loader(props) {
-    return (
-        <div className="loader" style={{margin:"15px",width:props.width,height:props.height}}></div>
-    )
-}
-
-export default Loader
